2157f7d9da554fb3591876ef9f32f7f1bfbd416d
numko99 <61965732+numko99@users.noreply.github.com>
2020-11-28 13:22:45 +0100

    dodan async

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,18 @@
 const router=require('express').Router();
 let User=require('../models/User.model');
 
-router.route('/').get((req,res)=>{
-    User.find().then(users=>res.json(users))
-                .catch(err=>res.status(400).json('Error'+err));
+// router.route('/').get((req,res)=>{
+//     User.find().then(users=>res.json(users))
+//                 .catch(err=>res.status(400).json('Error'+err));
+// })
+
+router.route('/').get( async (req,res)=>{
+    try{
+   const posts= await User.find();
+   res.json(posts);
+    }catch(err){
+        res.json('Error'+err)
+    }
 })
 
 router.route('/add').post((req,res)=>{
