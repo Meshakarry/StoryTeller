01ef634ab46774987e9adef91a56162895946a34
Mehmed Karadža <69600316+Meshakarry@users.noreply.github.com>
2020-11-30 18:23:10 +0100

    login

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -1,10 +1,25 @@
 import React from "react";
 
 
+
 export default function Login(){
     
     return (
-        <h1> mesha router king</h1>
+          <form>
+            <div className="form-group">
+           <label>Username</label>
+             <input type="text" placeholder="username" className='form-control'></input> 
+             <small className="form-text text-muted" >We will keep your privacy</small>
+             </div>
+             <div className="form-group">
+           <label>Password</label>
+             <input type="text" placeholder="password" className='form-control'></input> 
+            
+             </div>
+
+             
+             
+          </form>
     )
 
 
